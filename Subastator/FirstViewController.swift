f2e4aaa3e9ba4107a90aead1742daafe1f5aff67
xavipandis <xavipandis@xavipandiss-Mac.local>
2017-03-21 19:42:42 +0100

    Firebase

--- Subastator/FirstViewController.swift
+++ Subastator/FirstViewController.swift
@@ -7,17 +7,56 @@
 //
 
 import UIKit
+import FirebaseDatabase
+
+var ref: FIRDatabaseReference!
 
 class FirstViewController: UIViewController {
-
+    
+    var conditionRef: FIRDatabaseReference!
+    var userRef: FIRDatabaseReference!
+    
+    @IBOutlet weak var condition: UILabel!
+    @IBAction func login(_ sender: UIButton) {
+        /*let secondViewController: SecondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.show(secondViewController, sender: self)*/
+        
+        conditionRef.setValue("LOGIN")
+    }
+    @IBAction func reg(_ sender: AnyObject) {
+        conditionRef.setValue("REGISTRAR")
+        
+        let key = ref.child("user").childByAutoId().key
+        
+        let post = ["login": "dsad",
+                    "password": "dsad"]
+        let childUpdates = ["/user/\(key)": post]
+        ref.updateChildValues(childUpdates)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ref = FIRDatabase.database().reference()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        userRef = ref.child("user")
+        
+        userRef.observe(FIRDataEventType.value, with: { (snapshot) in
+            print(snapshot)
+            var data = snapshot.value
+            
+            /*let value = snapshot.value as? NSDictionary
+            var login = value?["login"] as? String ?? ""
+            var pass = value?["password"] as? String ?? ""
+            print("login " + login + ", password "+pass)*/
+            //self.condition.text = postDict as! String?
+        })
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
 
---
+++ Subastator/RegistroViewController.swift
@@ -0,0 +1,88 @@
+//
+//  RegistroViewController.swift
+//  Subastator
+//
+//  Created by xavipandis on 16/03/2017.
+//  Copyright © 2017 pandisbono. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class RegistroViewController: UIViewController {
+    
+    var userRef: FIRDatabaseReference!
+
+    @IBOutlet weak var inputUsuario: UITextField!
+    @IBOutlet weak var inputContraseña: UITextField!
+    @IBAction func btnCrearCuenta(_ sender: AnyObject) {
+        let usuario:String = inputUsuario.text!
+        let contraseña:String = inputContraseña.text!
+        var ok = false
+        if (usuario != nil || usuario != "") {
+            print("Usuario: "+usuario)
+            ok = true
+        }
+        else {
+          ok = false
+        }
+        
+        if(contraseña != nil || contraseña != ""){
+           print("Contraseña: "+contraseña)
+            ok = true
+        }else{
+            ok = false
+        }
+        
+        if ok == true {
+            let key = ref.child("user").childByAutoId().key
+            
+            print(usuario)
+            print(contraseña)
+            
+            let post = ["login": usuario,
+                        "password": contraseña]
+            let childUpdates = ["/user/\(key)": post]
+            ref.updateChildValues(childUpdates)
+        }
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        userRef = ref.child("user")
+        userRef.observe(FIRDataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            var login = value?["login"] as? String ?? ""
+            var pass = value?["password"] as? String ?? ""
+            print("login " + login + ", password "+pass)
+            //self.condition.text = postDict as! String?
+        })
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
