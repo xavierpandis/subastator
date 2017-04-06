28f2c9d26b6641f12ee64af5959602225fe3d354
jotabono <jotabono92@gmail.com>
2017-04-06 19:53:45 +0200

    Menu subastas añadido

---
+++ Subastator/CrearSubastaViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CrearSubastaViewController.swift
+//  Subastator
+//
+//  Created by xavipandis on 04/04/2017.
+//  Copyright © 2017 pandisbono. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class CrearSubastaViewController: UIViewController {
+    
+    var subastasRef: FIRDatabaseReference!
+
+    @IBOutlet weak var nombreSubasta: UITextField!
+    @IBOutlet weak var objeto: UITextField!
+    @IBOutlet weak var descripcionProducto: UITextView!
+    @IBOutlet weak var precioMinimo: UITextField!
+    @IBAction func crearSubasta(_ sender: UIButton) {
+        
+        let key = subastasRef.childByAutoId().key
+        
+        let nombre_subasta:String = nombreSubasta.text!
+        let objeto_subasta:String = objeto.text!
+        let descripcion_producto_subasta:String = descripcionProducto.text
+        let precio_minimo_subasta:String = precioMinimo.text!
+        
+        let subasta = ["nombre_subasta": nombre_subasta,
+                       "objeto_subasta": objeto_subasta,
+                       "descripcion_subasta": descripcion_producto_subasta,
+                       "precio_minimo_subasta": precio_minimo_subasta,
+                       "creador": "Javier Bono"]
+        
+        subastasRef.observe(FIRDataEventType.value, with: { (snapshot) in
+            let childUpdates = ["/subastas/\(key)": subasta]
+            ref.updateChildValues(childUpdates)
+            
+        })
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subastasRef = ref.child("subastas")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Subastator/PanelViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PanelViewController.swift
+//  Subastator
+//
+//  Created by xavipandis on 04/04/2017.
+//  Copyright © 2017 pandisbono. All rights reserved.
+//
+
+import UIKit
+
+class PanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
